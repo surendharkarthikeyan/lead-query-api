c5284883a407fd89f4fe5a9b684cfae541797523
Surendhar.K <surendhar.k@grootan.com>
2026-08-23 11:59:27 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/surendhar/leadquery/LeadQueryApiApplication.java
@@ -0,0 +1,13 @@
+package com.surendhar.leadquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadQueryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadQueryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surendhar/leadquery/LeadQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surendhar.leadquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadQueryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
